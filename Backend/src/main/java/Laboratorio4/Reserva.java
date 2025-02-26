916d63016455754c127f29ed7479a27c056dbc2f
AngeLCuervo <angel.cuervo@mail.escuelaing.edu.co>
2025-02-25 22:25:31 -0500

    Definicion modelo de datos

---
+++ Backend/src/main/java/Laboratorio4/Reserva.java
@@ -0,0 +1,15 @@
+package Laboratorio4;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reserva {
+
+    private String id;
+    private LocalTime horaInicicio;
+    private LocalDate fecha;
+    private boolean estado;
+    private String idUsuario;
+    private String idSalon;
+    private LocalTime horaFin;
+}
